9ed27ba6a7cba9c225effb3d3c2c46534c91e6f6
CBenni <levelcarnage@gmail.com>
2016-05-20 20:10:08 +0200

    Made the bot multi-server capable.

--- config.sample.js
+++ config.sample.js
@@ -1,9 +1,8 @@
 module.exports = {
     prefix: '!',
-    server_id: '1607470854xxxxxxxx', // Server to listen on
     client_id: '1782378161xxxxxxxx', // Client ID of the bot
     token: '', // Discord access token
-    lvtoken: '', // Log viewer access token
+    lvtokens: {}, // Log viewer access tokens per server id
     lvdomain: '', // Log viewer domain
     lvpublicdomain: '' // Log viewer domain to give as a response to !lv
 };
