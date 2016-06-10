23a456eca508a1e0763e390e0e41c35c66cdd4f3
3ventic <3ventic@gmail.com>
2016-06-10 23:34:12 +0200

    listen to Slack /command

--- config.sample.js
+++ config.sample.js
@@ -1,8 +1,7 @@
 module.exports = {
-    prefix: '!',
-    client_id: '1782378161xxxxxxxx', // Client ID of the bot
-    token: '', // Discord access token
-    lvtokens: {}, // Log viewer access tokens per server id
-    lvdomain: '', // Log viewer domain
-    lvpublicdomain: '' // Log viewer domain to give as a response to !lv
+    port: 8080,
+    token: '',
+    lvtoken: '',
+    lvdomain: '',
+    lvpublicdomain: ''
 };
--- lv.js
+++ lv.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
-var Discord = require('discord.js');
+var bodyParser = require('body-parser');
+var express = require('express');
+var http_app = express();
 var Request = require('request');
 var parseIrc = require('irc-message').parse;
 var config = require('./config.js');
@@ -8,51 +10,6 @@ var request = Request.defaults({
         'User-Agent': 'riotlv v' + require('./package.json').version
     }
 });
-
-const invitelink = 'https://discordapp.com/oauth2/authorize?client_id='
-    + config.client_id + '&scope=bot&permissions=19456';
-const defaults_file = './defaults.json';
-
-var channel_defaults = {};
-
-var client = new Discord.Client({
-    autoReconnect: true
-});
-
-client.on('ready', function () {
-    console.log("Ready");
-    client.setStatus('online', 'with butts');
-});
-
-client.on('message', function (message) {
-    let words = message.cleanContent.match(/(?:[^\s"]+|"[^"]*")+/g);
-    if (words && words[0].startsWith(config.prefix)) {
-        let cmd = words[0].substring(config.prefix.length);
-        if (commands[cmd]) {
-            words.shift();
-            commands[cmd](message, words);
-        }
-    }
-});
-
-client.on('warn', function (warn) {
-    console.error('WARN', warn);
-});
-
-client.on('error', function (error) {
-    console.error('ERROR', error);
-});
-
-client.loginWithToken(config.token, function (error) {
-    if (error) {
-        console.error("Couldn't login: ", error);
-        process.exit(15);
-    }
-});
-
-function getToken(message) {
-    return message.channel.server ? (config.lvtokens[message.channel.server.id] || "") : "";
-}
 
 function apiRequest(api, cb, errorcb) {
     request('http://' + config.lvdomain + '/api/' + api, function (error, response, body) {
@@ -79,104 +36,67 @@ function apiRequest(api, cb, errorcb) {
     });
 }
 
-var commands = {
-    help: function (message, words) {
-        sendReply(message, "command prefix: " + config.prefix + " - commands: " + Object.keys(commands).join(', '));
-    },
-    list: function (message, words) {
-        let token = getToken(message);
-        apiRequest('channels?token=' + token, function (data) {
-            let reply = "";
-            data.forEach(function (ch) {
-                if (reply.length > 1900) {
-                    sendReply(message, reply.slice(0, -2));
-                    reply = "";
-                }
-                reply += ch.name + ", ";
-            });
-            sendReply(message, reply.slice(0, -2));
-        }, function (error) {
-            sendReply(message, error);
-        });
-    },
-    setdefault: function (message, words) {
-        if (message.channel.permissionsOf(message.author).hasPermission('manageChannel')) {
-            let default_channel = words[0] || null;
-            if (default_channel !== null) {
-                channel_defaults[message.channel.id] = default_channel.toLowerCase();
-                sendReply(message, "default for this channel set to " + channel_defaults[message.channel.id]);
-            } else {
-                sendReply(message, "usage: !setdefault [channel name]");
-            }
-        } else {
-            sendReply(message, "shush, you don't have permission to do this!");
-        }
-    },
-    lv: function (message, words) {
-        let limit = words[2] || 10;
-        let channel = words[1] || channel_defaults[message.channel.id] || "riotgames";
-        let user = words[0] || null;
 
-        try {
-            limit = Math.min(parseInt(limit), 50);
-        } catch (e) {
-            sendReply(message, "limit must be a number");
-            return;
-        }
+function lookupLogs(words, cb) {
+    let limit = words[2] || 10;
+    let channel = words[1] || "riotgames";
+    let user = words[0] || null;
 
-        console.log("Log request for: `" + JSON.stringify({ user: words[0], channel: channel, limit: limit }) + "`");
-
-        if (user === null) {
-            sendReply(message, "usage: " + config.prefix + "lv <user> [channel] [limit]");
-            return;
-        }
-
-        let token = getToken(message);
-
-        apiRequest('logs/' + encodeURIComponent(channel) + "/?token=" + token
-                + "&nick=" + encodeURIComponent(user) + "&before=" + limit,
-        function (data) {
-            if (data.before) {
-                reply = "here are logs for **" + user + "** from **" + channel + "**\n\n```";
-                for (let i = data.before.length - 1; i >= 0; --i) {
-                    let index = data.before.length - 1 - i;
-                    let ircmsg = parseIrc(data.before[index].text);
-                    let line = "\n" + (new Date(data.before[index].time * 1000)).toISOString().replace('T', ' ').replace('Z', ' UTC')
-                        + " | " + ircmsg.params[1];
-                    if (reply.length + line.length > 1900) {
-                        reply += "\n```";
-                        sendReply(message, reply);
-                        reply = "\n```";
-                    }
-                    reply += line;
-                }
-                reply += "\n```";
-            }
-            else {
-                reply = "no data found.";
-            }
-            reply += '\nSee http://' + config.lvpublicdomain + '/' + encodeURIComponent(channel) + "/?user=" + encodeURIComponent(user);
-            sendReply(message, reply);
-        }, function (error) {
-            sendReply(message, error);
-        });
-    },
-    game: function (message, words) {
-        let game = words.join(' ') || "with butts";
-        client.setStatus('online', game);
-    },
-    invite: function(message, words) {
-        sendReply(message, invitelink);
+    try {
+        limit = Math.min(parseInt(limit), 50);
+    } catch (e) {
+        cb("limit must be a number");
+        return;
     }
-};
 
-function sendReply(message, reply) {
-    client.reply(message, reply, { tts: false }, function (error) {
-        if (error) {
-            console.error('WERROR', error);
+    console.log("Log request for: `" + JSON.stringify({ user: words[0], channel: channel, limit: limit }) + "`");
+
+    if (user === null) {
+        cb("usage: /lv <user> [channel] [limit]");
+        return;
+    }
+
+    apiRequest('logs/' + encodeURIComponent(channel) + "/?token=" + config.lvtoken
+            + "&nick=" + encodeURIComponent(user) + "&before=" + limit,
+    function (data) {
+        if (data.before) {
+            reply = "here are logs for " + user + " on " + channel + "\n\n```";
+            for (let i = data.before.length - 1; i >= 0; --i) {
+                let index = data.before.length - 1 - i;
+                let ircmsg = parseIrc(data.before[index].text);
+                let line = "\n" + (new Date(data.before[index].time * 1000)).toISOString().replace('T', ' ').replace('Z', ' UTC')
+                    + " | " + ircmsg.params[1];
+                reply += line;
+            }
+            reply += "\n```";
         }
+        else {
+            reply = "no data found.";
+        }
+        reply += '\nSee http://' + config.lvpublicdomain + '/' + encodeURIComponent(channel) + "/?user=" + encodeURIComponent(user);
+        cb(reply);
+    }, function (error) {
+        cb(error);
     });
 }
+
+http_app.use(bodyParser.json());
+http_app.use(bodyParser.urlencoded({ extended: true }));
+http_app.post('/command', function (req, res) {
+    let token = req.body.token;
+    if (token !== config.token) {
+        res.status(403).send('No access token');
+        return;
+    }
+    let text = req.body.text;
+    lookupLogs(text.match(/(?:[^\s"]+|"[^"]*")+/g), (msg) => {
+        res.status(200).send(msg);
+    });
+});
+
+var port = config.port || 8080;
+http_app.listen(port);
+console.log("Listening to HTTP on port " + port);
 
 process.on('SIGINT', function () {
     var exit_tasks_done = 0;
@@ -186,43 +106,6 @@ process.on('SIGINT', function () {
             process.exit();   
         }
     }
-    console.log("Logging out and saving data...");
-    client.logout(exitTaskCheck);
-    saveDefaults(exitTaskCheck);
-});
-
-function saveDefaults(cb) {
-    fs.access(defaults_file, fs.W_OK, (err) => {
-        if (err) {
-            console.error("No access to write", defaults_file);
-            cb();
-        } else {
-            fs.writeFile(defaults_file, JSON.stringify(channel_defaults), (err) => {
-                if (err) {
-                    console.error("Write error", defaults_file);
-                }
-                cb();
-            });
-        }
-    });
-}
-
-// Load defaults
-fs.access(defaults_file, fs.R_OK, (err) => {
-    if (err) {
-        console.error("No access", defaults_file);
-        saveDefaults(function () {});
-    } else {
-        fs.readFile(defaults_file, (err, data) => {
-            if (err) {
-                console.error("Read error", defaults_file, err);
-            } else {
-                try {
-                    channel_defaults = JSON.parse(data);
-                } catch (e) {
-                    console.error("JSON error", e);
-                }
-            }
-        });
-    }
+    console.log("Exiting..");
+    process.exit();
 });
