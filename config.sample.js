481b93dd0e08bcec1d216cca500b9cf83a6dd997
CBenni <CBenni@users.noreply.github.com>
2016-06-13 16:05:42 +0200

    Removed http:// from config info
    My bad :<

--- config.sample.js
+++ config.sample.js
@@ -3,6 +3,6 @@ module.exports = {
     client_id: '1782378161xxxxxxxx', // Client ID of the bot
     token: '', // Discord access token
     lvtokens: {}, // Log viewer access tokens per server id
-    lvdomain: 'http://cbenni.com', // Log viewer domain
-    lvpublicdomain: 'http://cbenni.com' // Log viewer domain to give as a response to !lv
+    lvdomain: 'cbenni.com', // Log viewer domain
+    lvpublicdomain: 'cbenni.com' // Log viewer domain to give as a response to !lv
 };
